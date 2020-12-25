39db4b76d48529d8b1842756603374225c2e143d
Jeshua Erickson <jeshua@gmail.com>
2020-12-25 13:14:00 -0500

    getting nonces now

--- artifacts/11a/OfficialNaughtyNiceBlockchainEducationPack/blockchain.py
+++ artifacts/11a/OfficialNaughtyNiceBlockchainEducationPack/blockchain.py
@@ -10,9 +10,13 @@ with open('official_public.pem','rb') as fh:
     official_public_key = RSA.importKey(fh.read())
 c2 = Chain(load=True, filename='blockchain.dat')
 print('C2: Block chain verify: %s' % (c2.verify_chain(official_public_key)))
-print(c2.blocks[0])
-c2.blocks[0].dump_doc(1)
 
+for var in list(range(1548)):
+    print(c2.blocks[var].block_data)
+    #c2.blocks[var].dump_doc(1)
+    me = var
+
+print(me)
 
 
 
